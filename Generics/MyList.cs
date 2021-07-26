d77d4ea9a9979e84549e1d06efe51667af954a07
erdem <erdemahmet750@gmail.com>
2021-07-26 19:54:55 +0300

    Proje dosyası ekle.

---
+++ Generics/MyList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MyList<T>
+    {
+        private static int objCount = 0;
+        public MyList()
+        {
+            objCount++;
+        }
+        public int Count
+        {
+            get
+            {
+                return objCount;
+            }
+            
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Generics
+{
+    
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("ankara", "izmir");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            List<Product> result2 = utilities.BuildList<Product>();
+
+        }
+    }
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+
+    }
+    class Product
+    {
+
+    }
+    interface IProductDal:IRepository<Product>
+    {
+        
+    }
+    interface IRepository<T>
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
