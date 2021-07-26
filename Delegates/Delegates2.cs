91686f8307f251241f17bd5008ccb54387e5b8ad
erdem <erdemahmet750@gmail.com>
2021-07-26 21:12:34 +0300

    Basit Delege ile static  metot çağırma

---
+++ Delegates/Delegates2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void BasitDelege();
+    public class Delegates2
+    {
+        public static void Topla()
+        {
+            Console.WriteLine("12+8=" + (12 + 8));
+        }
+        public static void Main2()
+        {
+           
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasitDelege obj = new BasitDelege(Delegates2.Topla);
+            obj();
+            //Vergiler v = new Vergiler();
+            //Console.WriteLine(v.GelirVergisi(v.BrütOku()));
+        }
+    }
+    //class Vergiler
+    //{
+    //    public double BrütOku()
+    //    {
+    //        Console.WriteLine("brüt gelir gir:");
+    //        return Double.Parse(Console.ReadLine());
+
+    //    }
+    //    public double GelirVergisi(double d)
+    //    {
+    //        if (d>20000)
+    //        {
+    //            return d * 0.40;
+    //        }
+    //        else
+    //        {
+    //            return d * 0.25;
+    //        }
+    //    }
+    //}
+}
